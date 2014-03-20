5a7eae0d8efe41e0199cc1a9620d312273da2729
Danilo Medeiros <danilo.medeiros@tjpi.jus.br>
2014-03-20 17:19:27 +0000

    bloqueio de comandos windows: start, alt+tab, alt+f4, esc, shift+est.

---
+++ EstacaoPonto/src/core/leitura/Operacao.java
@@ -0,0 +1,48 @@
+package core.leitura;
+
+import controllers.MainController;
+import core.DadosFrequentadores;
+import utils.KeyHook;
+import view.BloqueioTela;
+
+import java.util.Map;
+
+/**
+ * Created by Danilo on 18/03/14.
+ */
+public enum Operacao {
+    DESBLOQUEIO {
+        @Override
+        public void execute(String digital) {
+            try {
+                int idFre = LeitorDigital.getInstance().searchDigitalOnIndexSearchEngine(digital);
+                Map<Integer,String> administradores = DadosFrequentadores.getInstance().getAdministradores();
+                Boolean isAdministrador =  administradores.containsKey(idFre);
+                if(isAdministrador){
+                    BloqueioTela.getInstance().desbloquear();
+                }
+
+            }catch (Exception e){
+            }
+        }
+    },
+
+
+
+    REGISTRO_FREQUENCIA {
+        @Override
+        public void execute(String result) {
+            EventoLeitura.LEITURA_EM_ANALISE.process(MainController.INSTANCE.tela, null);
+            String digitalHash = "";
+            if (result != null){
+                digitalHash = result.toString();
+            }
+            VerificacaoDigitalService capturaDigitalService = new VerificacaoDigitalService(digitalHash);
+            capturaDigitalService.setOnSucceeded(new VerificacaoDigitalHandler());
+            capturaDigitalService.start();
+        }
+    };
+
+    public abstract void execute(String digital);
+
+}
