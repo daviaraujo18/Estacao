3fe4b70290ec5f6e7593cf1879f8871918b1a4b7
Danilo Medeiros <danilo.medeiros@tjpi.jus.br>
2014-03-20 15:33:38 +0000

    bloqueio de comandos windows: start, alt+tab, alt+f4, esc, shift+est.

---
+++ EstacaoPonto/src/core/EstacaoPonto.java
@@ -0,0 +1,99 @@
+package core;
+
+import controllers.MainController;
+import core.leitura.LeitorDigital;
+import javafx.application.Application;
+import javafx.event.EventHandler;
+import javafx.fxml.FXMLLoader;
+import javafx.geometry.Rectangle2D;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Dialogs;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.input.MouseEvent;
+import javafx.stage.Screen;
+import javafx.stage.Stage;
+import utils.KeyHook;
+import view.BloqueioTela;
+
+import java.util.Map;
+
+/**
+ * Classe principal da core.EstacaoPonto
+ * Nela sao definidos metodos que carregam a aplicacao em si
+ *
+ * @author Anderson Soares
+ */
+public class EstacaoPonto extends Application{
+
+    private Stage stage;
+    private static EstacaoPonto INSTANCE;
+
+    public EstacaoPonto(){
+        super();
+        this.INSTANCE = this;
+    }
+
+    public static EstacaoPonto getInstance(){
+        return INSTANCE;
+    }
+
+    /**
+     * Metodo que eh chamado antes de iniciar a aplicacao em si
+     * @throws Exception
+     */
+    @Override
+    public void init() throws Exception {
+        System.out.println("INITIALIZING");
+        super.init();
+    }
+
+    /**
+     * Metodo que eh chamado quando a aplicacao eh finalizada
+     *
+     * @throws Exception
+     */
+    @Override
+    public void stop() throws Exception {
+
+        System.out.println("STOPPING");
+        KeyHook.getInstance().unblockWindowsKey();
+        super.stop();
+    }
+
+    @Override
+    public void start(Stage palco) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("/resources/Main.fxml"));
+
+        this.stage = palco;
+        Scene scene = new Scene(root);
+        KeyHook.getInstance().blockWindowsKey();
+
+        stage.setScene(scene);
+        stage.setTitle("TJPI - Estação Ponto de Presença");
+
+        // Setando stage para maximized
+        Screen screen = Screen.getPrimary();
+        Rectangle2D bounds = screen.getBounds();
+
+        stage.setX(bounds.getMinX());
+        stage.setY(bounds.getMinY());
+        stage.setWidth(bounds.getWidth());
+        // -40 por causa da barra do Menu iniciar do windows
+        stage.setHeight(bounds.getHeight()-40);
+        // Setando tela cheia ON
+//        stage.setFullScreen(true);
+
+        BloqueioTela.getInstance().addClickTarget(root, MainController.INSTANCE.tela.webView);
+
+        stage.show();
+    }
+
+    public static void main(String... args){
+        launch(args);
+    }
+
+
+}
+
---
+++ EstacaoPonto/src/view/BloqueioTela.java
@@ -0,0 +1,91 @@
+package view;
+
+import controllers.MainController;
+import javafx.event.EventHandler;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.input.MouseEvent;
+import utils.KeyHook;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by Danilo on 14/03/14.
+ */
+public class BloqueioTela {
+
+    private static BloqueioTela instance = new BloqueioTela();
+    private boolean bloqueada = true;
+    EventHandler<MouseEvent> clickHandler;
+
+    private BloqueioTela(){
+        final EventHandler<MouseEvent> handler = new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent mouseEvent) {
+                if(bloqueada){
+                    MainController.INSTANCE.getCds().clickDesbloqueioTela = true;
+                }
+            }
+        };
+    }
+
+    public void addClickTarget(Node... nodes){
+        for (Node node : nodes){
+            node.setOnMouseClicked(clickHandler);
+            onKeyPressed(node);
+        }
+    }
+    public static BloqueioTela getInstance() {
+        return instance;
+    }
+
+    public void init(Parent root) {
+        onKeyPressed(root);
+    }
+
+    public void bloquear(){
+        KeyHook.getInstance().blockWindowsKey();
+        bloqueada = true;
+    }
+
+    public void desbloquear(){
+        KeyHook.getInstance().unblockWindowsKey();
+        bloqueada = false;
+    }
+
+    private void onMouseClicked(Scene root) {
+        final EventHandler<MouseEvent> handler = new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent mouseEvent) {
+                if(bloqueada){
+                    MainController.INSTANCE.getCds().clickDesbloqueioTela = true;
+                }
+            }
+        };
+
+        root.setOnMouseClicked(handler);
+    }
+
+    private void onKeyPressed(Node root) {
+        final EventHandler<KeyEvent> keyEventHandler = new EventHandler<KeyEvent>() {
+            public void handle(final KeyEvent keyEvent) {
+                if(keyEvent.getCode() == KeyCode.A){
+                    if(bloqueada){
+                        MainController.INSTANCE.getCds().clickDesbloqueioTela = true;
+                    }else{
+                        bloquear();
+                    }
+                }
+            }
+        };
+        root.setOnKeyPressed(keyEventHandler);
+    }
+
+    public boolean isBloqueada() {
+        return bloqueada;
+    }
+}
