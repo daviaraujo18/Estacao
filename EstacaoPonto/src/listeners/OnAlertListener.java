b410bada69ca09195c1f8143310478f4dfcb45ba
Danilo Medeiros <danilo.medeiros@tjpi.jus.br>
2014-04-22 16:56:41 +0000

    Refatoracao do onAlertListener

--- EstacaoPonto/src/listeners/OnAlertListener.java
+++ EstacaoPonto/src/listeners/OnAlertListener.java
@@ -5,14 +5,8 @@
 package listeners;
 
 import controllers.MainController;
-import core.DadosFrequentadores;
-import core.EstacaoPonto;
-import core.IntranetURLs;
-import core.RegistroWindows;
+
 import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.util.Calendar;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javafx.application.Platform;
@@ -20,7 +14,6 @@ import javafx.event.Event;
 import javafx.event.EventHandler;
 import javafx.scene.web.WebEngine;
 import javafx.scene.web.WebEvent;
-import utils.Log;
 
 /**
  *
@@ -35,11 +28,6 @@ import utils.Log;
  */
 public class OnAlertListener implements EventHandler {
 
-   
-
-    /*
-        REFACTOR TO CHAIN OF RESPONSABILITY
-     */
     @Override
     public void handle(Event t) {
 
@@ -47,111 +35,39 @@ public class OnAlertListener implements EventHandler {
 
         if (t instanceof WebEvent) {
             WebEvent event = (WebEvent) t;
-            String metodoAlerta = event.getData().toString();
-            if (metodoAlerta.equals("callRecuperarFrequentadores")
-                    && webEngine.getLocation().contains("tjpi/presenca/PontoDePresenca")) {       
-                Log.i("Iniciando download dos dados dos Frequentadores");
-                double inicioDownload = System.currentTimeMillis();
-                Object data = webEngine.executeScript("window.bdFrequencia");
+            String metodo = event.getData().toString();
+            boolean eventoTratado = false;
+            for(Operacao operacao : Operacao.values()){
+                if(operacao.verificarAplicabilidade(metodo, webEngine)){
+                    eventoTratado = true;
+                    operacao.execute(metodo, webEngine);
+                }
+            }
+            if(!eventoTratado && metodo.contains("comando")){
+                processarComando(webEngine);
+            }
+        }
+    }
 
-                String dataFixed = (String) data.toString().replace("\n", "");
-                //System.out.println("dados Baixados: " + dataFixed);
-                Log.i("Montando dados");
-                    DadosFrequentadores.getInstance().init(dataFixed);
-                double fimDownload = System.currentTimeMillis();            
-                Log.i("Montagem finalizada");
-                Log.i("Time elapsed: " + (fimDownload - inicioDownload) + " ms");
-                String horario = MainController.INSTANCE.getThreadRelogio().atualizarRelogio();
+    private void processarComando(WebEngine webEngine) {
+        Object comando = webEngine.executeScript("window.comando");
+        System.out.println("Comando estação: " + comando.toString());
+        if (!comando.toString().equals("undefined") && !comando.toString().equals("")) {
+            if (comando.toString().equals("FECHAR")) {
                 try {
-                    MainController.INSTANCE.atualizarHorario(horario);
-                } catch (IOException ex) {
-                    Logger.getLogger(OnAlertListener.class.getName()).log(Level.SEVERE, null, ex);
-                }
-                
-            } else if (metodoAlerta.equals("recuperarCodigoAtivacao")
-                    && webEngine.getLocation().contains("tjpi/presenca/RecuperarCodigoAtivacao")) {
-                Log.i("Recuperando CodigoDeAtivacao e setando no Registro do Windows");
+                    System.out.println("Fechou");
 
-                Object data = webEngine.executeScript("jQuery('#codigoAtivacao').val();");
+                    String path = new File("..").getCanonicalPath();
 
-                RegistroWindows.registrarCodigoAtivacao(data.toString());
+                    System.out.println("Tentando executar: "+path+"\\EstacaoPonto\\runOpenUpdate.bat");
+                    Process p =  Runtime.getRuntime().exec("cmd.exe /c start runOpenUpdate.bat",null,new File(path+"\\EstacaoPonto") );
 
-                webEngine.load(IntranetURLs.INICIALIZAR_PONTO + IntranetURLs.getCodigos());
-            } else if (metodoAlerta.contains("horarioServidorAtual")) {
-                String[] horario = metodoAlerta.split(":");
-
-                int dia = Integer.parseInt(horario[1]);
-                int mes = Integer.parseInt(horario[2]);
-                int ano = Integer.parseInt(horario[3]);
-                int hora = Integer.parseInt(horario[4]);
-                int minutos = Integer.parseInt(horario[5]);
-                Calendar dataServidor = Calendar.getInstance();
-                dataServidor.set(ano, mes, dia, hora, minutos);
-                MainController.INSTANCE.criarThreadRelogio(dataServidor);
-            } else if (metodoAlerta.equals("atualizarRelogioLocal")) {
-                System.out.println("Recebendo requisição para atualizar horário na página");
-                if (MainController.INSTANCE.getThreadRelogio() != null) {
-                    String horario = MainController.INSTANCE.getThreadRelogio().atualizarRelogio();
-                    try {
-                        System.out.println("Atualizando...");
-                        MainController.INSTANCE.atualizarHorario(horario);
-                    } catch (FileNotFoundException ex) {
-                        Logger.getLogger(OnAlertListener.class.getName()).log(Level.SEVERE, null, ex);
-                    } catch (IOException ex) {
-                        Logger.getLogger(OnAlertListener.class.getName()).log(Level.SEVERE, null, ex);
-                    }
-                }
-            } else if (metodoAlerta.equals("limparRegistosBatimentos")) {
-                try {
-                    MainController.INSTANCE.apagarRegistrosBatimentos();
-                } catch (IOException ex) {
-                    Logger.getLogger(OnAlertListener.class.getName()).log(Level.SEVERE, null, ex);
-                }
-            } else if (metodoAlerta.equals("Sincronizando")) {
-                System.out.println("ALERT Sincronizando...");
-            }
-            else if (metodoAlerta.contains("Sincronizar Agora"))
-            {
-                System.out.println("Sincronizando...");
-                try {
-                    MainController.INSTANCE.iniciarSincronizacao();
-                } catch (FileNotFoundException ex) {
-                    Logger.getLogger(OnAlertListener.class.getName()).log(Level.SEVERE, null, ex);
-                } catch (IOException ex) {
+                    Platform.exit();
+                    System.exit(0);
+                } catch (Exception ex) {
                     Logger.getLogger(OnAlertListener.class.getName()).log(Level.SEVERE, null, ex);
                 }
             }
-            else if(metodoAlerta.contains("vivooumorto"))
-            {
-                MainController.INSTANCE.iAmStillAlive();
-            }
-            else
-            {
-                if(metodoAlerta.contains("comando"))
-                {
-                    Object comando = webEngine.executeScript("window.comando");
-                    System.out.println("Comando estação: " + comando.toString());
-                    if (!comando.toString().equals("undefined") && !comando.toString().equals("")) {
-                        if (comando.toString().equals("FECHAR")) {
-                            try {
-                                System.out.println("Fechou");
-
-
-String path = new File("..").getCanonicalPath();
-
-System.out.println("Tentando executar: "+path+"\\EstacaoPonto\\runOpenUpdate.bat");
-Process p =  Runtime.getRuntime().exec("cmd.exe /c start runOpenUpdate.bat",null,new File(path+"\\EstacaoPonto") );               
-              
-                                                            Platform.exit();
-                                                            System.exit(0);
-                            } catch (Exception ex) {
-                                Logger.getLogger(OnAlertListener.class.getName()).log(Level.SEVERE, null, ex);
-                            }
-                        }
-                    }
-                }
-            }
-
         }
     }
 }
