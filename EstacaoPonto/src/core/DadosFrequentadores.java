eed95cc5c777a912e67cf1a111ef9fd69f0619b0
Danilo Medeiros <danilo.medeiros@tjpi.jus.br>
2014-04-29 14:10:41 +0000

    correcao de inicialização de estasção na intranet

--- EstacaoPonto/src/core/DadosFrequentadores.java
+++ EstacaoPonto/src/core/DadosFrequentadores.java
@@ -24,14 +24,14 @@ public class DadosFrequentadores  {
     private Map<Integer,String> administradores;
     private Map<Integer,String> mapaIdFotosFrequentadores;
     private String data;
-    
+
     public static DadosFrequentadores getInstance(){
         if(INSTANCE == null){
             INSTANCE = new DadosFrequentadores();
         }
         return INSTANCE;
     }
-    
+
     private DadosFrequentadores(){
 
     }
@@ -41,50 +41,50 @@ public class DadosFrequentadores  {
         Task task;
         task = new Task<Void>() {
 
-       @Override 
-       protected Void call() throws Exception {
-           setArrayFrequentadores(((String) DadosFrequentadores.getInstance().getData()).split("'"));
+            @Override
+            protected Void call() throws Exception {
+                setArrayFrequentadores(((String) DadosFrequentadores.getInstance().getData()).split("'"));
 
-            String[] frequentadores = getArrayFrequentadores();
+                String[] frequentadores = getArrayFrequentadores();
 
-            HashMap<String, String> hashFrequentadores = new HashMap();
-            setFrequentadores(new HashMap<Integer, String>());
-            setAdministradores(new HashMap<Integer, String>());
-            setMapaIdFotosFrequentadores(new HashMap<Integer, String>());
-            int total=0;
-            if (frequentadores.length > 0 && !frequentadores[0].isEmpty()) {
-                for (int i = 0; i < frequentadores.length; i++) {
-                    // id;matricula;nome;digital;foto
-                    String[] dados = frequentadores[i].split(";");
-                    String id = dados[0];
-                    String hashDigital = dados[3];
-                    String isAdmin = dados[5];
+                HashMap<String, String> hashFrequentadores = new HashMap();
+                setFrequentadores(new HashMap<Integer, String>());
+                setAdministradores(new HashMap<Integer, String>());
+                setMapaIdFotosFrequentadores(new HashMap<Integer, String>());
+                int total=0;
+                if (frequentadores.length > 0 && !frequentadores[0].isEmpty()) {
+                    for (int i = 0; i < frequentadores.length; i++) {
+                        // id;matricula;nome;digital;foto
+                        String[] dados = frequentadores[i].split(";");
+                        String id = dados[0];
+                        String hashDigital = dados[3];
+                        String isAdmin = dados[5];
 
 
-                    String foto = dados[4];
-                    String sexo = dados[6];
+                        String foto = dados[4];
+                        String sexo = dados[6];
 
-                    //matricula, nome, digital
-                    String dadosF = dados[1] + ";" + dados[2] + ";" + dados[4] + ";" + dados[6];// matricula;nome;foto;sexo
-                    getFrequentadores().put(Integer.parseInt(id), dadosF);
-                    if(isAdmin.equals("true")){
-                        getAdministradores().put(Integer.parseInt(id),dadosF);
+                        //matricula, nome, digital
+                        String dadosF = dados[1] + ";" + dados[2] + ";" + dados[4] + ";" + dados[6];// matricula;nome;foto;sexo
+                        getFrequentadores().put(Integer.parseInt(id), dadosF);
+                        if(isAdmin.equals("true")){
+                            getAdministradores().put(Integer.parseInt(id),dadosF);
+                        }
+                        hashFrequentadores.put(id, hashDigital);
+                        getmapaIdFotosFrequentadores().put(Integer.parseInt(id),foto);
+                        total = i;
                     }
-                    hashFrequentadores.put(id, hashDigital);
-                    getmapaIdFotosFrequentadores().put(Integer.parseInt(id),foto);
-                    total = i;
                 }
+                System.out.println("Total de frequentadores: "+total);
+                // Adiciona os dados ao NBio_SearchIndex
+                try {
+                    MainController.INSTANCE.getLeitorDigital().addDigitalToIndexSearch(hashFrequentadores);
+                } catch (Exception e) {
+                    Log.i("Leitor nao iniciado: " + e.getMessage());
+                }
+                return null;
             }
-            System.out.println("Total de frequentadores: "+total);
-        // Adiciona os dados ao NBio_SearchIndex
-        try {
-            MainController.INSTANCE.getLeitorDigital().addDigitalToIndexSearch(hashFrequentadores);
-        } catch (Exception e) {
-            Log.i("Leitor nao iniciado: " + e.getMessage());
-        }
-           return null;
-       }
-   };
+        };
 
         task.setOnSucceeded(new EventHandler<WorkerStateEvent>(){
 
@@ -96,16 +96,9 @@ public class DadosFrequentadores  {
 
                     @Override
                     public void handle(WorkerStateEvent t) {
-                         The.inserirJavascript(TelaPonto.INSTANCE.getWebEngine(), "aguardarDigital();");
-                        if(MainController.INSTANCE.getCds().isRunning())
-                        {
-                            MainController.INSTANCE.getCds().setUsarLeitor(false);
-                        }
-                        else
-                        {
-                            MainController.INSTANCE.getCds().start();
-                        }
-                       TelaPonto.INSTANCE.getSplitPanel().getDividers().get(1).setPosition(0.999);   
+                        The.inserirJavascript(TelaPonto.INSTANCE.getWebEngine(), "aguardarDigital();");
+                        MainController.INSTANCE.reiniciarCapturaDigital();
+                        TelaPonto.INSTANCE.getSplitPanel().getDividers().get(1).setPosition(0.999);
                         TelaPonto.INSTANCE.getBotaoCadastrarDigital().setVisible(false);
                         TelaPonto.INSTANCE.getBotaoAtualizarDigital().setVisible(false);
                         TelaPonto.INSTANCE.getProgressBar().setVisible(false);
@@ -118,15 +111,15 @@ public class DadosFrequentadores  {
                 TelaPonto.INSTANCE.getBotaoAtualizarDigital().setVisible(false);
                 TelaPonto.INSTANCE.getProgressBar().setVisible(true);
                 TelaPonto.INSTANCE.getLabelProgressBar().setVisible(true);
-                
+
                 novo.start();
                 TelaPonto.INSTANCE.getProgressBar().progressProperty().bind(downloads.progressProperty());
                 TelaPonto.INSTANCE.getLabelProgressBar().textProperty().bind(downloads.messageProperty());
             }
-            
+
         });
-        
-        new Thread(task).start();        
+
+        new Thread(task).start();
         System.out.println("----Fim.");
     }
 
