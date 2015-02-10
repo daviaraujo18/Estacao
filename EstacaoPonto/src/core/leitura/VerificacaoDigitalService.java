1d9eec8067a97c8ced780fac6b615c495f240b11
Daniel Evangelista <daniel.leite.chaves.evangelista@gmail.com>
2015-02-10 15:48:59 +0000

    Verifica em qual prédio o vinculado está associado e compara com os prédios permitido pela estação. Se algum for diferente mostra a tela de ressalva.

--- EstacaoPonto/src/core/leitura/VerificacaoDigitalService.java
+++ EstacaoPonto/src/core/leitura/VerificacaoDigitalService.java
@@ -44,21 +44,26 @@ public class VerificacaoDigitalService extends Service<Leitura>{
                         Map<Integer, String> mapaIdInfoFrequentadores = DadosFrequentadores.getInstance().getFrequentadores();
                         String[] dados = mapaIdInfoFrequentadores.get(id).split(";");
                         String localTrabalho = dados[4];
-                        System.out.println("localtrabalho: "+localTrabalho);
-                        String prediosIds = MainController.INSTANCE.prediosIds;
-                        System.out.println("executado");
-                        System.out.println("prediosIds: "+prediosIds.toString());
-                        String [] prediosIdsArray = prediosIds.toString().split(";");
-                        System.out.println("comparando predios");
                         boolean definido =false;
-                        for (String predioId : prediosIdsArray)
+                        System.out.println("localtrabalho: "+localTrabalho);
+                        if (!localTrabalho.equals("0"))
                         {
-                            if (localTrabalho.equals(predioId))
+                            String prediosIds = MainController.INSTANCE.prediosIds;
+                            System.out.println("executado");
+                            System.out.println("prediosIds: "+prediosIds.toString());
+                            String [] prediosIdsArray = prediosIds.toString().split(";");
+                            System.out.println("comparando predios");
+                            
+
+                            for (String predioId : prediosIdsArray)
                             {
-                                System.out.println("predio igual: "+localTrabalho + " "+predioId);
-                                resultado = EventoLeitura.DIGITAL_RECONHECIDA;
-                                definido = true;
-                                break;
+                                if (localTrabalho.equals(predioId))
+                                {
+                                    System.out.println("predio igual: "+localTrabalho + " "+predioId);
+                                    resultado = EventoLeitura.DIGITAL_RECONHECIDA;
+                                    definido = true;
+                                    break;
+                                }
                             }
                         }
                         if (!definido)
@@ -66,6 +71,7 @@ public class VerificacaoDigitalService extends Service<Leitura>{
                             System.out.println("fim da comparação");
                             resultado = EventoLeitura.DIGITAL_RECONHECIDA_RESSALVA_PREDIO;
                         }
+                        
                     }else{
                         resultado = EventoLeitura.DIGITAL_NAO_RECONHECIDA;
                     }
